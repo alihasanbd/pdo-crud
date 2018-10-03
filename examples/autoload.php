ee8102d0bc069b2d90260922fc80f3aa5a56184d
Ali Hasan <alihasan.iubat@gmail.com>
2018-10-03 10:45:55 +0600

    updated example autoloader

--- examples/autoload.php
+++ examples/autoload.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once(__DIR__ .'/../src/Database.php');
 spl_autoload_register(function ($class){
 	$class = str_replace('Kodeio\Database\\', '', $class);
 	$file = __DIR__ .'/../src/'. $class .'.php';
@@ -8,6 +9,6 @@ spl_autoload_register(function ($class){
 	}
 });
 
-Kodeio\Database\Conn::global(
+Kodeio\Database::conn(
 	'localhost', 'root', '', 'phpcrud_test', 3307
 );
